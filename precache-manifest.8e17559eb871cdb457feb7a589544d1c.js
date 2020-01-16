ea192630e87d9ae3e3d2a817b160feb54ac19e02
Nandan <nandankmrjha@gmail.com>
2020-01-16 15:48:56 +0530

    Updates

---
+++ precache-manifest.8e17559eb871cdb457feb7a589544d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c089afbf46dcd9bc0bbfe41990f214",
+    "url": "/evernote/index.html"
+  },
+  {
+    "revision": "54a7484c7be21b248e9a",
+    "url": "/evernote/static/css/main.1be67a60.chunk.css"
+  },
+  {
+    "revision": "a3722dbfc1fd1ecc7a49",
+    "url": "/evernote/static/js/2.c3a3d40c.chunk.js"
+  },
+  {
+    "revision": "01c28c1afb365be4d5040eb84e3dc52e",
+    "url": "/evernote/static/js/2.c3a3d40c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54a7484c7be21b248e9a",
+    "url": "/evernote/static/js/main.347a6990.chunk.js"
+  },
+  {
+    "revision": "3332310c52d3094302e8",
+    "url": "/evernote/static/js/runtime-main.1508f1a2.js"
+  }
+]);
